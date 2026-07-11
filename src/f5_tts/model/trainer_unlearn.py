9fd4132235e3ba1be71bda206bdcd65b8bde30d3
Cata400 <cata0310@gmail.com>
2026-07-11 13:57:59 +0000

    added different computation variations for SGU loss

--- src/f5_tts/model/trainer_unlearn.py
+++ src/f5_tts/model/trainer_unlearn.py
@@ -17,6 +17,9 @@ from torch.utils.data import (
     SequentialSampler,
     WeightedRandomSampler,
 )
+from torchjd import autojac
+from torchjd.aggregation import UPGrad
+from torchjd.autojac import jac_to_grad
 from tqdm import tqdm
 
 import wandb
@@ -171,6 +174,16 @@ class TrainerUnlearn:  # TODO add info logger
 
         self.unlearn_params = unlearn_params
         self.forget_speakers = forget_speakers
+
+        if not self.unlearn_params.get("always_retain_and_forget", False) and self.unlearn_params.get(
+            "use_torchjd", False
+        ):
+            raise ValueError(
+                "Unlearning configuration error: 'always_retain_and_forget' must be True when 'use_torchjd' is enabled."
+            )
+
+        if self.unlearn_params.get("always_retain_and_forget", False) and self.unlearn_params.get("use_torchjd", False):
+            self.aggregator = UPGrad()
 
     @property
     def is_main(self):
@@ -1180,30 +1193,73 @@ class TrainerUnlearn:  # TODO add info logger
                     mel_lengths_retain = batch["mel_lengths_retain"]
                     mel_lengths_forget = batch["mel_lengths_forget"]
 
-                    do_forget = torch.rand((1,)).item() < self.unlearn_params["forget_ratio"]
-                    if do_forget and mel_spec_forget.numel() > 0:  # if there is at least one forget sample
-                        mel_spec_concat = torch.cat([mel_spec_forget, mel_spec_retain], dim=1)
-                        text_inputs_concat = [
-                            text_inputs_forget[i] + " " + text_inputs_retain[i] for i in range(len(text_inputs_retain))
-                        ]
-                        mel_lengths_concat = mel_lengths_forget + mel_lengths_retain
+                    if self.unlearn_params.get("always_retain_and_forget", False):
+                        if mel_spec_forget.numel() > 0:  # if there is at least one forget sample
+                            mel_spec_concat = torch.cat([mel_spec_forget, mel_spec_retain], dim=1)
+                            text_inputs_concat = [
+                                text_inputs_forget[i] + " " + text_inputs_retain[i]
+                                for i in range(len(text_inputs_retain))
+                            ]
+                            mel_lengths_concat = mel_lengths_forget + mel_lengths_retain
 
-                        loss, cond, pred = self.model.forward_unlearn_SGU(
-                            mel_spec_concat,
-                            text=text_inputs_concat,
-                            lens=mel_lengths_concat,
-                            retain_lens=mel_lengths_retain,
-                            noise_scheduler=self.noise_scheduler,
-                        )
-                    else:
-                        loss, cond, pred = self.model.forward_unlearn(
+                            forget_loss, forget_cond, forget_pred = self.model.forward_unlearn_SGU(
+                                mel_spec_concat,
+                                text=text_inputs_concat,
+                                lens=mel_lengths_concat,
+                                retain_lens=mel_lengths_retain,
+                                noise_scheduler=self.noise_scheduler,
+                            )
+                        else:
+                            forget_loss = torch.tensor(0.0, device=self.accelerator.device)
+
+                        retain_loss, retain_cond, retain_pred = self.model.forward_unlearn(
                             mel_spec_retain,
                             text=text_inputs_retain,
                             lens=mel_lengths_retain,
                             noise_scheduler=self.noise_scheduler,
                             forget=False,
                         )
-                    self.accelerator.backward(loss)
+
+                        if not self.unlearn_params.get("use_torchjd", False):
+                            # type 1
+                            loss = (1 - self.unlearn_params["forget_ratio"]) * retain_loss + self.unlearn_params[
+                                "forget_ratio"
+                            ] * forget_loss
+                            # type 2
+                            # loss = retain_loss + self.unlearn_params["forget_ratio"] * forget_loss
+                    else:
+                        do_forget = torch.rand((1,)).item() < self.unlearn_params["forget_ratio"]
+                        if do_forget and mel_spec_forget.numel() > 0:  # if there is at least one forget sample
+                            mel_spec_concat = torch.cat([mel_spec_forget, mel_spec_retain], dim=1)
+                            text_inputs_concat = [
+                                text_inputs_forget[i] + " " + text_inputs_retain[i]
+                                for i in range(len(text_inputs_retain))
+                            ]
+                            mel_lengths_concat = mel_lengths_forget + mel_lengths_retain
+
+                            loss, cond, pred = self.model.forward_unlearn_SGU(
+                                mel_spec_concat,
+                                text=text_inputs_concat,
+                                lens=mel_lengths_concat,
+                                retain_lens=mel_lengths_retain,
+                                noise_scheduler=self.noise_scheduler,
+                            )
+                        else:
+                            loss, cond, pred = self.model.forward_unlearn(
+                                mel_spec_retain,
+                                text=text_inputs_retain,
+                                lens=mel_lengths_retain,
+                                noise_scheduler=self.noise_scheduler,
+                                forget=False,
+                            )
+
+                    if not self.unlearn_params.get("use_torchjd", False):
+                        self.accelerator.backward(loss)
+                    else:
+                        trainable_params = [p for p in self.model.parameters() if p.requires_grad]
+                        autojac.backward([retain_loss, forget_loss], inputs=trainable_params)
+                        jac_to_grad(trainable_params, self.aggregator)
+                        loss = retain_loss + forget_loss  # for logging purposes
 
                     if self.max_grad_norm > 0 and self.accelerator.sync_gradients:
                         self.accelerator.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
